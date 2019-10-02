5626358b81cb7a7451a7b3e56530943c25f88505
Diogo Lessa <diogolessacosta@gmail.com>
2019-10-02 07:33:07 -0300

    Removendo container

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 #$REPOSITORY_NAME="dlessa/nodejs-docker-app"
 
 docker container kill nodejs-docker-app > /dev/null 2>&1
-docker image rm --force dlessa/nodejs-docker-app > /dev/null 2>&1
+docker container rm nodejs-docker-app > /dev/null 2>&1
 docker container run -d -p 4000:4000 --name nodejs-docker-app dlessa/nodejs-docker-app:latest
