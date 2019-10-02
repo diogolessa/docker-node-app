a7370816a95f1aefabf37ac9769d785b781db465
Diogo Lessa <diogolessacosta@gmail.com>
2019-10-02 07:31:15 -0300

    Ajustando script shell 2

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-$APP_NAME="nodejs-docker-app"
-$REPOSITORY_NAME="dlessa/nodejs-docker-app"
+#$APP_NAME="nodejs-docker-app"
+#$REPOSITORY_NAME="dlessa/nodejs-docker-app"
 
-docker container kill $(APP_NAME) > /dev/null 2>&1
-docker image rm --force $(REPOSITORY_NAME) > /dev/null 2>&1
-docker container run -d -p 4000:4000 --name $(APP_NAME) $(REPOSITORY_NAME):latest
+docker container kill nodejs-docker-app > /dev/null 2>&1
+docker image rm --force dlessa/nodejs-docker-app > /dev/null 2>&1
+docker container run -d -p 4000:4000 --name nodejs-docker-app dlessa/nodejs-docker-app:latest
